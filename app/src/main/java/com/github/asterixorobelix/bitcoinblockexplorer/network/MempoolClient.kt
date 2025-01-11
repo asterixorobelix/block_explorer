25e1bed8ef407c2182a2d230ed1b1b77ac4c3a40
Nathan <nathanstasin@zohomail.eu>
2025-01-11 15:21:03 +0200

    documentation

--- app/src/main/java/com/github/asterixorobelix/bitcoinblockexplorer/network/MempoolClient.kt
+++ app/src/main/java/com/github/asterixorobelix/bitcoinblockexplorer/network/MempoolClient.kt
@@ -6,10 +6,14 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 
+/**
+ * Retrieve info from the network from mempool
+ */
 class MempoolClient(private val httpClient: HttpClient) {
 
     /**
-     * Returns details on the past 15 blocks with fee and mining details in an extras field
+     * Returns details on the last 15 blocks with fee and mining details in an extras field
+     * @return list of [BlockResponse]
      */
     suspend fun getBlocks(): List<BlockResponse> {
         val response = httpClient.get("${MEMPOOL_BASE_URL}blocks").body<List<BlockResponse>>()
